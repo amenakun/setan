b9078b6e63e21ca9eb6e89f4adb1e1920cf84f7b
amenakun <50862294+amenakun@users.noreply.github.com>
2019-05-27 01:37:23 +0700

    add files via upload
    hello

---
+++ setan.sh
@@ -0,0 +1,79 @@
+clear
+figlet FCM | lolcat
+
+echo "======================================
+# created by : root 4m3na
+# facebook   : Amena Gans
+# thanks to  : FCM & IDN
+
+=======================================" | lolcat
+sleep 3
+
+echo "{1} SpamWaCall
+{2} Dark Fb
+{3} Ddos Attack
+{4} CCTV
+{5} YahooCloning
+{6} Deface" | lolcat
+echo "pilih nomernya B0$: " | lolcat
+read nomor
+
+if [ $nomor = 1 ]
+then
+clear
+pkg install git
+pkg install php
+git clone https://github.com/siputra12/prank
+cd prank
+php call.php
+fi
+
+if [ $nomor = 2 ]
+then
+clear
+pkg install python2
+pkg install git
+git clone https://github.com/ZeDDParker/dark-fb
+cd dark-fb
+python2 install.py
+fi
+
+if [ $nomor = 3 ]
+then
+clear
+pkg install git
+pkg install python
+git clone http://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+fi
+
+if [ $nomor = 4 ]
+then
+clear
+pkg install git
+pip2 install requests
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+
+if [ $nomor = 5 ]
+then
+pkg install pip2
+pip2 install requests
+pip2 install mechanize
+pkg install git
+git clone https://github.com/GUNAWAN18ID/cloning-yahoo.git
+cd cloning-yahoo
+python2 yahoo.py
+fi
+
+if [ $nomor = 6 ]
+then
+apt install bash
+apt install git
+git clone https://github.com/21D4N404/Defacer
+cd Defacer
+sh ICA.sh
+fi
